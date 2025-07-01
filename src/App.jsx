f2887a50a4ed25d716c7b2fa87c3721cc2dd28b2
ebarbosa27 <ebarbosa5449@gmail.com>
2025-07-01 15:03:05 -0500

    added episode list and details to app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import EpisodeDetails from "./episodes/EpisodeDetails";
+import EpisodeList from "./episodes/EpisodeList";
+import { tvShows } from "./shows/data";
+import { useState } from "react";
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+  const [selectedShow, setSelectedShow] = useState(tvShows[0]);
   return (
     <>
       <header>
         <p>React TV</p>
       </header>
-      <main></main>
+      <main>
+        <EpisodeList
+          name={selectedShow.name}
+          episodes={selectedShow.episodes}
+          selectedEpisode={selectedEpisode}
+          setSelectedEpisode={setSelectedEpisode}
+        />
+        <EpisodeDetails episode={selectedEpisode} />
+      </main>
     </>
   );
 }
