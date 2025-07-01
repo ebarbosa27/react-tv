eee62cddb64313d042820155042570c817446016
ebarbosa27 <ebarbosa5449@gmail.com>
2025-07-01 15:32:31 -0500

    moved details page to show details instead of app

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  if (!show) {
+    return <p>Select a show to view episodes</p>;
+  }
+
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      {selectedEpisode ? (
+        <EpisodeDetails episode={selectedEpisode} />
+      ) : (
+        <p>Select an episode for more details.</p>
+      )}
+    </div>
+  );
 }
